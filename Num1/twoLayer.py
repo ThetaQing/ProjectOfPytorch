fe40d501b27da5853799fad2f77eaed7c3ed0b94
ThetaQing <csusq2016@163.com>
2020-05-23 15:35:14 +0800

    完全用Numpy实现一个两层神经网络

---
+++ Num1/twoLayer.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 21:39:32 2020
+完全用Numpy实现一个两层神经网络
+@author: Administrator
+"""
+
+
+import numpy as np
+N, D_in, H, D_out = 64, 1000, 100, 10
+# 随机创建一些训练数据
+x = np.random.randn(N, D_in)
+y = np.random.randn(N, D_out)
+
+w1 = np.random.randn(D_in, H)
+w2 = np.random.randn(H, D_out)
+
+learning_rate = 1e-6
+for it in range(500):
+    # 前向传输 Forward pass
+    h = x.dot(w1) # 得到 N * H
+    h_relu = np.maximum(h,0) # N * H
+    y_pred = h_relu.dot(w2) # N * D_out
+    
+    # conpute loss 计算损失MSE均方误差
+    loss = np.square(y_pred - y).sum()
+    print(it,loss)
+    
+    # Backward pass
+    # computer the gradient,都是相对于loss求导
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.T.dot(grad_y_pred)
+    grad_h_relu = grad_y_pred.dot(w2.T)
+    grad_h = grad_h_relu.copy()
+    grad_h[h < 0] = 0
+    grad_w1 = x.T.dot(grad_h)
+    
+    
+    # update weights of w1 and w2
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
+
